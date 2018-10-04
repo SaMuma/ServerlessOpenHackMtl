4feb2090450c33a5332b50a73e5c742aa34f5b9e
Sarah Muma <sarahmuma@outlook.com>
2018-10-04 14:28:25 -0400

    test cd

--- bfyoc.ratings/Settings.cs
+++ bfyoc.ratings/Settings.cs
@@ -8,7 +8,6 @@ namespace BFYOC.Ratings
 {
     class Settings
     {
-
         public const string GetUserUrl = "https://serverlessohuser.trafficmanager.net/api/GetUser";
         public const string GetProductsUrl = "https://serverlessohproduct.trafficmanager.net/api/GetProducts";
         public const string GetProductUrl = "https://serverlessohproduct.trafficmanager.net/api/GetProduct";
